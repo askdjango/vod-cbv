bb02009aa6a2f1655c446f1e8e3a8d3eca31e189
Chinseok Lee <allieuslee@gmail.com>
2017-07-01 09:06:37 +0900

    blog.models.Post 모델에 updated_at 필드 추가

--- blog/models.py
+++ blog/models.py
@@ -4,4 +4,5 @@ from django.db import models
 class Post(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
+    updated_at = models.DateTimeField(auto_now=True)
 
